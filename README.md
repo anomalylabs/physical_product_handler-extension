abaae75bfa22f8524fd9db9f5301423cc72dc819
Ryan Thompson <service@aiwebsystems.com>
2016-04-12 14:49:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Standard Product Type Extension
+
+The standard product type for the Catalog module.
---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Standard Product',
+    'name'        => 'Standard Product Type Extension',
+    'description' => 'The standard product type for the Catalog module.'
+];
